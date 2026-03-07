4b4e7ab062fa91a44b15aa571d52d2531608b564
NourhanDeifSayed <s-nourhan.sayed@zewailcity.edu.eg>
2026-03-07 11:24:31 -0800

    Clear all files from frontend branch

--- frontend/Test2-main/assistify/apps/orders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PlaceOrderView, OrderListView, OrderDetailView, ReviewCreateView
-
-urlpatterns = [
-    path("", PlaceOrderView.as_view(), name="order-place"),
-    path("list/", OrderListView.as_view(), name="order-list"),
-    path("<str:order_number>/", OrderDetailView.as_view(), name="order-detail"),
-    path("reviews/create/", ReviewCreateView.as_view(), name="review-create"),
-]
--- frontend/assistify_integrated/src/services/chatService.js
+++
@@ -1,1 +0,0 @@
-export { sendChatMessage } from "./api";
